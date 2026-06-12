0e88936f3b75d3fbbcb9109a49ecddb9f4b15eae
NIGAM KUMAR <120089977+Nigam27@users.noreply.github.com>
2026-06-12 16:38:29 +0530

    Assignment_12_june

--- dev/Day_4/Age_in_Months.py
+++
@@ -1,7 +0,0 @@
-# Program to convert age in years to months
-
-age_years = int(input("Enter age in years: "))
-
-age_months = age_years * 12
-
-print(f"Age in months: {age_months}")
--- dev/Day_4/Convert_cls_frh.py
+++
@@ -1,5 +0,0 @@
-# Program to convert Celsius to Fahrenheit
-
-celsius = float(input("Enter temperature in Celsius: "))
-fahrenheit = (celsius * 9 / 5) + 32
-print(f"Temperature in Fahrenheit: {fahrenheit}")
